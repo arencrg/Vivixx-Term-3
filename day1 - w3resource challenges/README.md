5dc491edfa23e245427ea8b4e5ca3f4e9b239027
Aren Cariaga <clarence13cariaga@gmail.com>
2017-09-14 12:15:43 +0800

    Reorganizing the folders

---
+++ day1 - w3resource challenges/README.md
@@ -0,0 +1,28 @@
+### day1 - w3resource challenges
+
+The exercises were from [W3Resource](http://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php)
+
+- day 1-1.html -- date printer
+
+- day 1-2.html -- temperature converter
+
+- day 1-3.html -- capitalization program (not sure if it's working right)
+
+- day 1-4.html -- is 50/ equals 50
+
+- day 1-5.html -- alphabet soup (not sure if it's working right)
+
+- day 1-6.html -- no remainder division
+
+- day 1-7.html -- how many vowels (might be lacking)
+
+- day 1-8.html -- checks if two numbers are within a given range
+
+----------
+
+The challenge for the day was:
+
+Lets help Grail become a blogger. She needs tools.
+It is a must for her to deliver not only high quality content but with in exact measurements.
+
+- day1challenge.html -- blogger tool, where to eat + generate the hashtag
---
+++ day4 - node/index.js
@@ -0,0 +1,48 @@
+const HTTP = require('http');
+const URL = require('url');
+
+const { jsonResponse } = require('./utils'); //use the exported function
+
+const server = HTTP.createServer((req, res) => {
+    try {
+      const urlComponents = URL.parse(req.url);
+      switch (urlComponents.pathname) { //which route shall we handle?
+
+        case '/':
+          res.end('Good job! You just wrote your first Node server!')
+        break;
+
+      case '/example.json':
+        jsonResponse(res, { //JSON response
+              example: true,
+              random: Math.random(),
+              time: (new Date()).toUTCString(),
+        });
+      break;
+
+      case '/error':
+      null.somethingWrong();
+      break;
+
+      default: //all others
+        res.writeHead(404); //not found error
+        res.end('You lost, buddy?\nPage not found: ' + urlComponents.pathname);
+        break;
+      }
+
+    }
+    catch(error) { //handle internal exceptions, so we don't just catch
+      console.error(error); //for our server logs
+      res.writeHead(500); //to tell the user 'internal server error'
+      res.end('Girl, mali!');
+    }
+});
+
+server.on('clientError', (err, socket) => {
+  //send raw HTTP 400 down the socket
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
+});
+
+server.listen(80, function() {
+    console.log('Started on port', 80);
+});
