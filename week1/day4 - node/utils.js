92b12320aa9697df222d7a9494608a5dde6e3718
Aren Cariaga <clarence13cariaga@gmail.com>
2017-09-14 12:18:23 +0800

    Reorganizing the folders

--- week1/day4 - node/utils.js
+++
@@ -1,7 +0,0 @@
-function jsonResponse(res, json) {
-  const jsonString = JSON.stringify(json);
-  res.setHeader('Content-Type', 'application/json');
-  res.end(jsonString);
-}
-
-module.exports = { jsonResponse: jsonResponse };
--- week3/day1 - trying out react/README.md
+++
@@ -1,1 +0,0 @@
-Today we learned about React, and I tried to make it work on (Codepen.io)[https://codepen.io/arencrg/pen/eGONYr?editors=1010]
